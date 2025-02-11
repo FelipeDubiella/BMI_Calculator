f7b67da342119fd3c052975385137889e6bf85e0
Felipe Dubiella de Oliveira <Dubiella2@icloud.com>
2025-02-11 12:51:11 -0300

    Initial Commit

---
+++ app/src/main/java/com/felipedubiella/bmi_calculator/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.felipedubiella.bmi_calculator
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val edtHeight = findViewById<TextInputEditText>(R.id.edtHeight)
+        val edtWeight = findViewById<TextInputEditText>(R.id.edtWeight)
+
+        val btnCalculate = findViewById<Button>(R.id.btnCalculate)
+
+        btnCalculate.setOnClickListener {
+
+            val height = edtHeight.text.toString()
+            val Weight = edtWeight.text.toString()
+
+            println("Peso e altura: " + height + " " + Weight)
+
+
+        }
+
+    }
+}
